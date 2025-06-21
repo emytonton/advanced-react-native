6ed2947cb61512584ccfd4c515644f13133e5a2e
Emilly Paiva <emillypaiva3260@gmail.com>
2025-06-21 19:41:56 -0300

    feat: navigate between pages

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Emi</Text>
-    </View>
+import {NavigationContainer} from '@react-navigation/native'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+
+import Home from "./src/pages/home";
+import Sobre from "./src/pages/Sobre";
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App(){
+  return(
+  <NavigationContainer>
+    <Stack.Navigator>
+
+      <Stack.Screen name="Home"  component={Home} options={{
+        title: 'Tela inicio',
+        headerStyle:{
+          backgroundColor: '#000'
+        },
+        headerTintColor:'#FFF'
+      
+      }}
+      
+      />
+
+
+
+      <Stack.Screen name="Sobre" component={Sobre}  />
+
+    </Stack.Navigator>
+  </NavigationContainer>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
+}
