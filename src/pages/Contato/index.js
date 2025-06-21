7716ecae1d37accea9f8ffd4039823a825385ca3
Emilly Paiva <emillypaiva3260@gmail.com>
2025-06-21 20:51:57 -0300

    feat: function handleHome to back to home page

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+import { useNavigation, StackActions } from "@react-navigation/native";
 
 
 export default function Contato(){
+
+    const navigation = useNavigation();
+
+    function handleHome(){
+        navigation.dispatch(StackActions.popToTop())
+    }
+
+
     return(
         <View>
+            <Text>Pag de contatos</Text>
+            <Button  title="Voltar para home" onPress={handleHome}/>
         </View>
     );
 }
