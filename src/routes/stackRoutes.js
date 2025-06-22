34ecf242ddbd17d088e29eea0a20b95843efd325
Emilly Paiva <emillypaiva3260@gmail.com>
2025-06-22 13:20:13 -0300

    feat: Stack route

---
+++ src/routes/stackRoutes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from "../pages/home";
+
+const Stack = createNativeStackNavigator();
+
+export default function StackRputes(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen 
+            name="Home"
+            component={Home}
+            />
+        </Stack.Navigator>
+    )
+}
