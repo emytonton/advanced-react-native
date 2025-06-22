07d02f139b294fda9b6be2d8751a963047e3430d
Emilly Paiva <emillypaiva3260@gmail.com>
2025-06-22 12:55:47 -0300

    feat: icons to tab navigator

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import {NavigationContainer} from '@react-navigation/native'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Feather from 'react-native-vector-icons/Feather';
 
 
 
@@ -20,16 +21,32 @@ export default function App(){
       <Tab.Screen 
       name="Home"
       component={Home}
+      options={{
+        //tabBarLabel: 'Inicio'
+        tabBarIcon: ({color, size}) => {
+          return <Feather  name = "home" color={color} size = {size} />
+        }
+      }}
       />
 
       <Tab.Screen 
       name="Sobre"
       component={Sobre}
+      options={{
+        tabBarIcon: ({color, size}) => {
+          return <Feather  name = "file-text" color={color} size = {size} />
+        }
+      }}
       />
 
       <Tab.Screen 
       name="Contato"
       component={Contato}
+      options={{
+        tabBarIcon: ({color, size}) => {
+          return <Feather  name = "phone-call" color={color} size = {size} />
+        }
+      }}
       />
     </Tab.Navigator>
   
